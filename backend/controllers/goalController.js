d937a1ba54c8ea7be4a02c820bded3af0b23c8d2
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-15 21:55:46 +0500

    basic goal CRUD setup done

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,17 @@
+const getGoals = (req, res) => {
+
+}
+
+const postGoal = (req, res) => {
+
+}
+
+const updateGoal = (req, res) => {
+
+}
+
+const delGoal = (req, res) => {
+
+}
+
+module.exports = {getGoals, postGoal, updateGoal, delGoal}
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/goalController')
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/", (req, res) => {
+
+});
+
+
+router.put("/:id", (req, res) => {
+
+});
+
+router.delete("/:id", (req, res) => {
+
+});
+
+module.exports = router
