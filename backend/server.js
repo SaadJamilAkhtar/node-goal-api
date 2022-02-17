a8378c75883a63ae81728da29e30257cde598658
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-02-17 05:06:13 +0500

    mongo setup done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
